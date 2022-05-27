1ab7c4950e4df7556011763656615c1d00ce7940
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2022-05-27 09:43:11 -0300

    Initial commit

---
+++ src/main/java/com/examplo/calculador/SomaDelegate.java
@@ -0,0 +1,15 @@
+package com.examplo.calculador;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class SomaDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        Long a = (Long) execution.getVariable("valorA");
+        Long b = (Long) execution.getVariable("valorB");
+
+        execution.setVariable("resultadoFinal", a + b);
+    }
+}
